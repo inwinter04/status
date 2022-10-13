7b8808f946d6159868e9d2a790f7268bb39bff31
冬天冬天W <57310036+inwinter04@users.noreply.github.com>
2022-10-13 17:49:46 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '冬天的监测站',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792848430-668a92c296c0a4602ace4582',
+    'm792848421-92c2daf2464d0d7c6612c92a',
+    'm792848424-4dc39f47571bccefc3d3269a',
+    'm792848435-4770ffa53ab18a06e6cbb521',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://www.iamdt.cn/'
+    },
+    {
+      text: 'Pages',
+      url: 'https://pages.iamdt.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/inwinter04'
+    },
+  ],
+};
